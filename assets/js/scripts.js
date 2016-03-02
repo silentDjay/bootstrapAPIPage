f2c39b10cade9e7e326e06655c29c09f237373b5
Jeremy Smith <smithjeremym@gmail.com>
2016-03-02 17:31:30 -0500

    get clicked country returning

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,26 +15,43 @@ $(document).ready( function () {
     });
 
 
-  //this gets the latitude and longitude of a user's click
-  google.maps.event.addListener(map, "click", function (event) {
-
-    var clickedSpot = {position: event.latLng, map: map};
+    //this gets the latitude and longitude of a user's click
+    google.maps.event.addListener(map, "click", function (event) {
+    //create an object with the clickevent's latlng information within it
+      var clickedSpot = {position: event.latLng, map: map};
     console.log(clickedSpot);
+    var posish = clickedSpot.position;
+    console.log(posish);
+    //fetch the latitude of the click
+      var latitude = clickedSpot.position.lat();
+    console.log(latitude);
+    //fetch the longitude of the click
+      var longitude = clickedSpot.position.lng();
+    console.log(longitude);
 
-    var lat = clickedSpot.position.lat();
-    console.log(lat);
+    //this function below might help me get the country name based on the latLng coordinates of the click
+    //this is what we are going for: results[7].formatted_address: "United States"
+    // you'll find the answer here: https://developers.google.com/maps/documentation/javascript/geocoding#ReverseGeocoding
 
-    var lng = clickedSpot.position.lng();
-    console.log(lng);
+      var geocoder = new google.maps.Geocoder;
+      var latlng = {lat: latitude, lng: longitude};
+      geocoder.geocode({'location': latlng}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[4]) {
+            var countryClicked = results[4].formatted_address;
+            console.log("you clicked ");
+            console.log(countryClicked);
+          } else {
+            window.alert("You didn't click on a country");
+          }
+        } else {
+          window.alert('Geocoder failed due to: ' + status);
+        }
 
-    //lat and lng is available in e object
-    // var latLng = e.latLng;
-    // var clickLat = e.latlng.lat();
-    // var clickLng = e.latlng.lng();
-    // console.log(clickLat);
-    // console.log(clickLng);
+      });
 
-  });
+    });
+
   }
 
 });
