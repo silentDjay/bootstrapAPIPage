f371f5ba21ceb1bf0105e549f3a88ffca42a4446
Jeremy Smith <smithjeremym@gmail.com>
2016-03-02 23:18:32 -0500

    wire up spotify api to search for tracks based on clicked location

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready( function () {
   "use strict";
 
+// I followed the instructions here to link up to the Spotify API: https://github.com/jmperez/spotify-web-api-js
+// var spotifyApi = new SpotifyWebApi();
+// spotifyApi.setAccessToken('<here_your_access_token>');
 
+// var Spotify = require('spotify-web-api-js');
+// var s = new Spotify();
 
 // this stackoverflow helped me get my google maps call working: http://stackoverflow.com/questions/34466718/googlemaps-does-not-load-on-page-load
 
@@ -11,7 +16,10 @@ $(document).ready( function () {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 42.29, lng: -85.585833},
       zoom: 3,
-      // mapTypeId: google.maps.MapTypeId.SATELLITE
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      disableDefaultUI: true,
+      zoomControl: true,
+      draggableCursor: 'crosshair'
     });
 
 
@@ -29,9 +37,8 @@ $(document).ready( function () {
       var longitude = clickedSpot.position.lng();
     console.log(longitude);
 
-    //this function below might help me get the country name based on the latLng coordinates of the click
-    //this is what we are going for: results[7].formatted_address: "United States"
-    // you'll find the answer here: https://developers.google.com/maps/documentation/javascript/geocoding#ReverseGeocoding
+    //this function below gets the country name based on the latLng coordinates of the click
+    // this documentation provided all of my answers: https://developers.google.com/maps/documentation/javascript/geocoding#ReverseGeocoding
 
       var geocoder = new google.maps.Geocoder;
       var latlng = {lat: latitude, lng: longitude};
@@ -39,13 +46,36 @@ $(document).ready( function () {
         if (status === google.maps.GeocoderStatus.OK) {
           if (results[4]) {
             var countryClicked = results[4].formatted_address;
+            console.log(results[4]);
             console.log("you clicked ");
             console.log(countryClicked);
+            $(".modal").modal('show');
+            $(".modal").html("You clicked on " + countryClicked);
+
+            var placeArray = countryClicked.split(" ");
+            console.log(placeArray);
+            countryClicked = placeArray.join("+");
+            console.log(countryClicked);
+
+            // here comes the spotify stuff
+            $.getJSON( 'https://api.spotify.com/v1/search/?q='+countryClicked+'&type=track', function (data) {
+
+                // this does kinda work!
+                console.log(data.tracks.items);
+
+            });
+//     });
+// };
+
           } else {
-            window.alert("You didn't click on a country");
+            $(".modal").modal('show');
+            $(".modal").html("No tunes about that place. Click somewhere else!");
+            // window.alert("No tunes about this place. Click somewhere else!");
           }
         } else {
-          window.alert('Geocoder failed due to: ' + status);
+          $(".modal").modal('show');
+          $(".modal").html("No tunes about that place. Click somewhere else!");
+          // window.alert("No tunes about this place. Click somewhere else!");
         }
 
       });
